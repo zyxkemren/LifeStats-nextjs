e3621e1f605a76a7138a21296b931c05c64d5d29
zyxkemren <daffa.adli212@gmail.com>
2025-07-04 21:56:23 +0700

    2

---
+++ src/components/ui/RadarChart.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Chart as ChartJS, RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend } from "chart.js";
+import { Radar } from "react-chartjs-2";
+
+ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend);
+
+type Props = {
+  data: any;
+  options: any;
+};
+
+export default function RadarChart({ data, options }: Props) {
+  return (
+    <div className="w-full m-[1rem] p-4 rounded-xl shadow">
+      <Radar data={data} options={options} />
+    </div>
+  );
+}
